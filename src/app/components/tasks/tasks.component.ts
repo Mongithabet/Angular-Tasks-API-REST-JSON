4e453899b06d7d1690acbeff0b3b42e2c475df29
Mongithabet <stechnopc@yahoo.fr>
2020-04-20 20:15:29 +0100

    Form

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
+import { Task } from 'src/app/models/task';
 
 @Component({
   selector: 'app-tasks',
@@ -6,10 +8,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-
-  constructor() { }
+  editForm = false;
+  showForm=false;
+  myTask: Task={
+    label:'',
+    completed:false
+  }
+tasks: Task[] = [];
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.getTasks();
   }
-
+ getTasks(){
+   this.taskService.findAll()
+   .subscribe((tasks)=>{this.tasks= tasks})
+ }
+ 
+deleteTask(id){
+  this.taskService.delete(id)
+  .subscribe(()=>{
+    this.tasks=this.tasks.filter(task =>task.id !=id)
+  })
 }
+persistTask(){
+  this.taskService.persist(this.myTask)
+  .subscribe((task) =>{
+   this.tasks = [task, ...this.tasks],
+   this.resetTask();
+  });
+}
+resetTask(){
+  this.myTask={
+    label:'',
+    completed:false
+  }
+}
+ToggleCompleted(task){
+  this.taskService.completed(task.id, task.completed)
+  .subscribe(()=>{
+    task.completed= !task.completed
+  })
+}
+editTask(task){
+
+    this.myTask=task;
+    this.editForm=true;
+    }
+    
+    updateTask(){
+      this.taskService.update(this.myTask)
+      .subscribe((task)=>{
+        this.resetTask();
+        this.editForm=false;
+
+      })
+    }
+}
+
